ed01d9cd722dbb2988e949abbd31c032849674a5
Victor Guerra Veloso <victorgvbh@gmail.com>
2020-10-13 18:01:12 -0300

    FAILED ATTEMPT: grow unidirectionally, bigger "left" subarray never tested

--- src/StockExchange.cpp
+++ src/StockExchange.cpp
@@ -9,21 +9,12 @@ int calculate(int *start, const int *end, int fee) {
     bool ascending = false;
     int left = 0;
     int right = 0;
-    int mid = 0;
-    int *pivot = nullptr;
     while (cursor <= end) { //TODO: break recursively
         if (*cursor <= *(cursor - 1)) {
             if (ascending) {
-                ascending = false;
-                if(pivot != nullptr) {
-                    mid = calculate(pivot, cursor - 1, fee);
-                    right = calculate(cursor, end, fee);
-                    break;
-                }
-                else {
-                    pivot = cursor;
-                    left = calculate(start, pivot - 1, fee);
-                }
+                left = calculate(start, cursor - 1, fee);
+                right = calculate(cursor, end, fee);
+                break;
 
             }
         } else {
@@ -32,23 +23,7 @@ int calculate(int *start, const int *end, int fee) {
         ++cursor;
     }
     int delta = (*end) - (*start);
-    if (cursor > end) {
-        if (pivot != nullptr) {
-            right = calculate(pivot, end, fee);
-            return std::max({delta - fee, left + right, 0});
-        }
-        else {
-            return std::max({delta - fee, 0});
-        }
-    }
-    int left_aggregated = *(cursor-1) - *start - fee;
-    int right_aggregated = *end - *pivot - fee;
-    return std::max({
-        delta - fee,
-        left + mid + right,
-        left_aggregated + right,
-        right_aggregated + left,
-        0});
+    return std::max({delta - fee, left + right, 0});
 }
 
 int StockExchange::calculateMaxProfit() {
